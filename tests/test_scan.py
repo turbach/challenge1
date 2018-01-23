ada0a4403ca25f1a5b1163b6539f063173bcb5e4
Andrey Portnoy <aportnoj@gmail.com>
2018-01-23 13:47:21 -0800

    add parametrized speed test
    quick_scan is required to be faster than slow scan under all parameter
    combinations

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -34,27 +34,29 @@ def test_quick_scan_correctness_random_data(n_samples, window, max_amplitude):
 
     assert (slow_result == quick_result).all()
 
-def test_quick_scan_speed_random_data():
+
+@pytest.mark.parametrize("n_samples", [100, 1000, 10000])
+@pytest.mark.parametrize("window", [1, 3, 10, 30])
+@pytest.mark.parametrize("max_amplitude", [10, 70])
+def test_quick_scan_speed_random_data(n_samples, window, max_amplitude):
     '''Test execution time of quick scan against execution time of slow_scan,
        use randomly generated data. Succeeds if quick_scan is faster than 
        slow_scan, fails otherwise.'''
 
-    data = generate_data(1000, 1)
-    window = 30
-    maxamp = 70 
+    data = generate_data(n_samples, n_channels=1)
 
     # decorator wizardry to be able to time a function with arguments
-    def timeit_decorator(func, data, window, maxamp):
+    def timeit_decorator(func, data, window, max_amplitude):
         def func_no_args():
-            return func(data, window, maxamp)
+            return func(data, window, max_amplitude)
         return func_no_args
 
-    n_runs = 5
+    n_runs = 3
     
-    slow_scan_no_args = timeit_decorator(slow_scan, data, window, maxamp)
+    slow_scan_no_args = timeit_decorator(slow_scan, data, window, max_amplitude)
     slow_scan_time = timeit(slow_scan_no_args, number=n_runs)
 
-    quick_scan_no_args = timeit_decorator(quick_scan, data, window, maxamp)
+    quick_scan_no_args = timeit_decorator(quick_scan, data, window, max_amplitude)
     quick_scan_time = timeit(quick_scan_no_args, number=n_runs)
 
     assert quick_scan_time < slow_scan_time
