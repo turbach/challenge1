0085f778eb0dd66865c2e4efaa53461943ea4ab6
Andrey Portnoy <aportnoj@gmail.com>
2018-01-23 16:05:41 -0800

    refactor, add docstrings

--- tools.py
+++ tools.py
@@ -1,96 +1,165 @@
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
-from scipy import signal
 import matplotlib.pyplot as plt
 import bottleneck as bn
 
 
-def quick_scan(channel, window, max_amplitude):
+def quick_scan(data, win_len, max_excursion):
+    """Scan data for amplitude excursions within windows of fixed size.
+
+    Within each window, the edges of the excursion subinterval are detected,
+    data within each subinterval is marked as 'bad'.
+
+    Parameters
+    ----------
+    data : np.array, shape=(n,)
+        1-D data array of length n
+    win_len : positive int
+        size of the rolling window to be tested for excursions
+    max_excursion : positive int
+        maximum peak-to-peak range allowed
+
+    Returns
+    -------
+    result : np.array, shape=(n,), dtype=bool
+        array of bools, same length as the input data; True at the indices of
+        the bad stretches, False otherwise.
+
+
+    """
 
     # make sure the data is a flat array
-    channel = channel.reshape(-1)
-    n = len(channel)
+    data = data.reshape(-1)
 
     # prepare result array
-    result = np.zeros(n)
-    
+    n = len(data)
+    result = np.full(n, False)
+
     # detect amplitude excursions
-    bad_windows_mask = get_bad_windows_mask(channel, window, max_amplitude)
+    bad_windows_mask = get_bad_windows_mask(data, win_len, max_excursion)
 
     # return if no excursions
-    if bad_windows_mask.any() == False:
+    if not bad_windows_mask.any():
         return result
 
     # subset windows where excursions occur
-    flagged_windows = strided_view(channel, window)[bad_windows_mask]
-
-    # get relative bounds of bad intervals
-    rel_bad_starts, rel_bad_stops = get_rel_bad_bounds(flagged_windows, window)
-
-    # calculate absolute (within channel) indices of subintervals of 'bad' data
-    absolute_offsets = np.arange(n - window + 1)
-    abs_bad_starts = absolute_offsets[bad_windows_mask] + rel_bad_starts
-    abs_bad_stops  = absolute_offsets[bad_windows_mask] + rel_bad_stops + 1
+    bad_windows = strided_view(data, win_len)[bad_windows_mask]
 
     # get locations of bad data
-    index_array = get_bad_indices(abs_bad_starts, abs_bad_stops)
+    bad_indices = get_bad_indices(n, win_len, bad_windows, bad_windows_mask)
 
     # set flags at 'bad' data locations
-    result[index_array] = 1
-    
+    result[bad_indices] = True
+
     return result
 
 
-def get_bad_windows_mask(channel, window, max_amplitude):
+def get_bad_windows_mask(data, win_len, max_excursion):
+    """Get boolean mask corresponding to windows with 'bad' data.
+
+    Parameters
+    ----------
+    data : 1D array, shape=(n,)
+        1-D data array of length n
+    win_len : positive int
+        size of the rolling window to be tested for excursions
+    max_excursion : positive int
+        maximum peak-to-peak range allowed
+
+    Returns
+    -------
+
+    bad_windows_mask : np.array, shape=(n-win_len+1,), dtype=bool
+        array of bools, same length as the number of rolling windows; True at
+        the indices of bad windows, False otherwise
+
+    """
 
     # calculate amplitude within rolling windows
-    maxima = bn.move_max(channel, window=window)
-    minima = bn.move_min(channel, window=window)
-    amplitude = (maxima - minima)[window-1:]
+    maxima = bn.move_max(data, window=win_len)
+    minima = bn.move_min(data, window=win_len)
+    amplitude = (maxima - minima)[win_len-1:]
 
     # detect amplitude excursions
-    bad_windows_mask = amplitude > max_amplitude
+    bad_windows_mask = amplitude > max_excursion
 
     return bad_windows_mask
 
 
-def get_rel_bad_bounds(flagged_windows, window):
+def get_bad_indices(n, win_len, bad_windows, bad_windows_mask):
+    """Create an array containing indices of bad data.
+
+    Parameters
+    ----------
+    n : positive int
+        length of data
+    win_len : positive int
+        size of the rolling window
+    bad_windows : np.array, shape=(n_bad_windows, win_len)
+        2D numeric array of views of windows where bad data was found
+    bad_windows_mask : np.array, shape=(n-win_len+1,), dtype=bool
+        array of bools, same length as the number of rolling windows; True at
+        the indices of bad windows, False otherwise
+
+    Returns
+    -------
+    bad_indices : np.array, shape=(n,), dtype=int
+        array of ints indicating the locations of bad data
+
+    """
 
     # detect rightmost extrema within flagged windows
-    rightmost_min = window - 1 - bn.move_argmin(flagged_windows, window)[:, window-1]
-    rightmost_max = window - 1 - bn.move_argmax(flagged_windows, window)[:, window-1]
+    rmin = win_len - 1 - bn.move_argmin(bad_windows, win_len)[:, win_len-1]
+    rmax = win_len - 1 - bn.move_argmax(bad_windows, win_len)[:, win_len-1]
 
     # detect leftmost extrema within flagged windows
-    flipped_flagged_windows = np.flip(flagged_windows, axis=1)
-    leftmost_min = bn.move_argmin(flipped_flagged_windows, window)[:, window-1]
-    leftmost_max = bn.move_argmax(flipped_flagged_windows, window)[:, window-1]
+    flipped_bad_windows = np.flip(bad_windows, axis=1)
+    lmin = bn.move_argmin(flipped_bad_windows, win_len)[:, win_len-1]
+    lmax = bn.move_argmax(flipped_bad_windows, win_len)[:, win_len-1]
 
     # calculate relative (within windows) indices of subintervals of 'bad' data
-    rel_bad_starts = np.minimum(leftmost_min, leftmost_max)
-    rel_bad_stops  = np.maximum(rightmost_min, rightmost_max)
+    rel_bad_starts = np.minimum(lmin, lmax).astype(int)
+    rel_bad_stops = np.maximum(rmin, rmax).astype(int)
 
-    return rel_bad_starts, rel_bad_stops
-
-
-def get_bad_indices(abs_bad_starts, abs_bad_stops):
+    # calculate absolute (within channel) indices of subintervals of 'bad' data
+    abs_offsets = np.arange(n - win_len + 1)
+    abs_bad_starts = abs_offsets[bad_windows_mask] + rel_bad_starts
+    abs_bad_stops = abs_offsets[bad_windows_mask] + rel_bad_stops + 1
 
     # construct list of paired [start, stop] indices, detect unique
-    slices = np.array((abs_bad_starts, abs_bad_stops)).astype(int).T
+    slices = np.array((abs_bad_starts, abs_bad_stops)).T
     unique_slices = np.unique(slices, axis=0)
 
     # construct array index from paired indices
     bad_interval_indices = np.array([np.arange(*s) for s in unique_slices])
-    index_array = np.unique(np.concatenate(bad_interval_indices))
+    bad_indices = np.unique(np.concatenate(bad_interval_indices))
 
-    return index_array
+    return bad_indices
 
 
 def strided_view(data, win_len):
-    
+    """Create an array of rolling window views (no copying).
+
+    For more information, see the docs of numpy.lib.stride_tricks.as_strided.
+
+    Parameters
+    ----------
+    data : np.array, shape=(n,)
+        1-D data array of length n
+    win_len : positive int
+        size of the rolling window to be tested for excursions
+
+    Returns
+    -------
+    strided_view : np.array, shape=(n-win_len+1, win_len)
+        a view of the data 'through' rolling windows of width win_len
+    """
+
     shape = (len(data) - win_len + 1, win_len)
     stride, = data.strides
     strides = (stride, stride)
-    return as_strided(data, shape=shape, strides=strides)
+    strided_view = as_strided(data, shape=shape, strides=strides)
+    return strided_view
 
 
 def slow_scan(data, win_len, max_excursion):
